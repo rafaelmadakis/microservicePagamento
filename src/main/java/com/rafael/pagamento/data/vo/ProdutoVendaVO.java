d08790066a50cda355a30e0f81ee45f92078831a
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-05-05 13:07:44 -0300

    repository e VO's

---
+++ src/main/java/com/rafael/pagamento/data/vo/ProdutoVendaVO.java
@@ -0,0 +1,46 @@
+package com.rafael.pagamento.data.vo;
+
+import java.io.Serializable;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.rafael.pagamento.entity.ProdutoVenda;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@JsonPropertyOrder({ "id", "idProduto", "quantidade" })
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class ProdutoVendaVO extends RepresentationModel<ProdutoVendaVO> implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@JsonProperty("id")
+	private Long id;
+
+	@JsonProperty("idProduto")
+	private Long idProduto;
+
+	@JsonProperty("quantidade")
+	private Integer quantidade;
+
+	public static ProdutoVendaVO create(ProdutoVenda produtoVenda) {
+		return new ModelMapper().map(produtoVenda, ProdutoVendaVO.class);
+	}
+
+}
--- src/main/java/com/rafael/pagamento/entity/Produto.java
+++ src/main/java/com/rafael/pagamento/entity/Produto.java
@@ -1,5 +1,32 @@
 package com.rafael.pagamento.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "venda")
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Produto {
+	
+	@Id
+	private Long id;
+	
+	@Column(name = "estoque", nullable = false, length = 10)
+	private Integer estoque;
+	
 
 }
