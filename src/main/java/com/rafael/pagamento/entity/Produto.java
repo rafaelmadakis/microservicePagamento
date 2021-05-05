d37cab87185c9414a13bd8a9bca8cf2360515801
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-05-05 12:40:22 -0300

    Criando microservise de pagamento

---
+++ src/main/java/com/rafael/pagamento/entity/Produto.java
@@ -0,0 +1,5 @@
+package com.rafael.pagamento.entity;
+
+public class Produto {
+
+}
---
+++ src/main/java/com/rafael/pagamento/entity/ProdutoVenda.java
@@ -0,0 +1,51 @@
+package com.rafael.pagamento.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "produto_venda")
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ProdutoVenda implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "idProduto", nullable = false, length = 255)
+	private Long idProduto;
+
+	@Column(name = "quantidade", nullable = false, length = 10)
+	private Integer quantidade;
+
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_venda")
+	private Venda venda;
+}
